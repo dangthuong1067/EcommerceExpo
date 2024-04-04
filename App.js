6e293845aa67431b9616945fa32fe5a3df5c6b6f
Thuong <dangthuong1067@gmail.com>
2024-04-04 20:52:15 +0700

    Phần không bị lỗi

--- App.js
+++ App.js
@@ -1,107 +1,86 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
-
-import React, { useEffect, useMemo, useState } from 'react'
+// import React, { useEffect, useMemo, useState } from 'react'
 import { View, Text, ActivityIndicator } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+//  import { createDrawerNavigator } from '@react-navigation/drawer';
+// // import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Provider, useDispatch, useSelector } from 'react-redux';
-import Tab from './src/components/tab/tab.component';
-import Home from './src/screens/home/home.component';
-import Cart from './src/screens/cart/cart.component';
-import FavoriteProduct from './src/screens/favorite-product/favorite-product.component';
-import Profile from './src/screens/profile/profile.component';
-import Category from './src/screens/category/category.component';
-import Chat from './src/screens/chat/chat.component';
+// // import Tab from './src/components/tab/tab.component';
+// // import Home from './src/screens/home/home.component';
+// // import Cart from './src/screens/cart/cart.component';
+// // import FavoriteProduct from './src/screens/favorite-product/favorite-product.component';
+// // import Profile from './src/screens/profile/profile.component';
+// // import Category from './src/screens/category/category.component';
+// // import Chat from './src/screens/chat/chat.component';
 import Login from './src/screens/login/login.component';
 import Register from './src/screens/register/register.component';
 import ForgotPassword from './src/screens/forgot-password/forgot-password.component';
 import store from './src/redux/store';
+// import { Text } from 'react-native';
 import SplashScreen from './src/screens/splash-screen/splashScreen.component';
 
 const Stack = createStackNavigator();
-const DrawerStack = createDrawerNavigator();
-const BottomTab = createBottomTabNavigator();
+// const DrawerStack = createDrawerNavigator();
+// const BottomTab = createBottomTabNavigator();
 
-function AppDrawerStack() {
-  return (
-    <DrawerStack.Navigator
-      screenOptions={{ headerShown: false, swipeEnabled: false }}
-      drawerContent={props => <DrawerView {...props} />}>
-      <DrawerStack.Screen name='AppBottomStack' component={AppBottomStack} />
-    </DrawerStack.Navigator>
-  )
-}
+// function AppDrawerStack() {
+//   // return (
+//   //   <DrawerStack.Navigator
+//   //     screenOptions={{ headerShown: false, swipeEnabled: false }}
+//   //     drawerContent={props => <DrawerView {...props} />}>
+//   //     <DrawerStack.Screen name='AppBottomStack' component={AppBottomStack} />
+//   //   </DrawerStack.Navigator>
+//   // )
+// }
 
-function DrawerView() {
-  return (
-    <View>
-      <Text> Drawer View </Text>
-    </View>
-  )
-}
+// function DrawerView() {
+//   return (
+//     <View>
+//       <Text> Drawer View </Text>
+//     </View>
+//   )
+// }
 
-function AppBottomStack() {
-  return (
-    <BottomTab.Navigator
-      tabBar={props => <Tab {...props} />}
-      screenOptions={{ headerShown: false, }}
-    >
-      <BottomTab.Screen
-        name='Home'
-        component={Home}
-        options={{ icon: 'home-outline', tabBarLabel: 'Trang chủ' }}
-      />
-      <BottomTab.Screen
-        name='Category'
-        component={Category}
-        options={{ icon: 'copy-outline', tabBarLabel: 'Danh mục' }}
-      />
+// function AppBottomStack() {
+//   return (
+//     <BottomTab.Navigator
+//       tabBar={props => <Tab {...props} />}
+//       screenOptions={{ headerShown: false, }}
+//     >
+//       <BottomTab.Screen
+//         name='Home'
+//         component={Home}
+//         options={{ icon: 'home-outline', tabBarLabel: 'Trang chủ' }}
+//       />
+//       <BottomTab.Screen
+//         name='Category'
+//         component={Category}
+//         options={{ icon: 'copy-outline', tabBarLabel: 'Danh mục' }}
+//       />
 
-      <BottomTab.Screen
-        name='Cart'
-        component={Cart}
-        options={{ icon: 'cart-outline', tabBarLabel: 'Giỏ hàng' }}
-      />
-      <BottomTab.Screen
-        name='Fovorite'
-        component={FavoriteProduct}
-        options={{ icon: 'heart-outline', tabBarLabel: 'Yêu thích' }}
-      />
-      <BottomTab.Screen
-        name='Chat'
-        component={Chat}
-        options={{ icon: 'chatbox-outline' }}
-      />
-      <BottomTab.Screen
-        name='Profile'
-        component={Profile}
-        options={{ icon: 'person-outline' }}
-      />
-    </BottomTab.Navigator>
-  )
-}
+//       <BottomTab.Screen
+//         name='Cart'
+//         component={Cart}
+//         options={{ icon: 'cart-outline', tabBarLabel: 'Giỏ hàng' }}
+//       />
+//       <BottomTab.Screen
+//         name='Fovorite'
+//         component={FavoriteProduct}
+//         options={{ icon: 'heart-outline', tabBarLabel: 'Yêu thích' }}
+//       />
+//       <BottomTab.Screen
+//         name='Chat'
+//         component={Chat}
+//         options={{ icon: 'chatbox-outline' }}
+//       />
+//       <BottomTab.Screen
+//         name='Profile'
+//         component={Profile}
+//         options={{ icon: 'person-outline' }}
+//       />
+//     </BottomTab.Navigator>
+//   )
+// }
 
 const AuthStack = () => (
   <Stack.Navigator screenOptions={{ headerShown: false }}>
@@ -115,7 +94,7 @@ const ProtectedStack = () => (
   <Stack.Navigator
     screenOptions={{ headerShown: false }}
   >
-    <Stack.Screen name="AppDrawerStack" component={AppDrawerStack} />
+    {/* <Stack.Screen name="AppDrawerStack" component={AppDrawerStack} /> */}
   </Stack.Navigator>
 )
 
@@ -140,7 +119,10 @@ const Navigation = () => {
   }
   return (
     <NavigationContainer>
-      {rendering}
+      {/* {rendering} */}
+      <AuthStack />
+      {/* <SplashScreen /> */}
+      {/* <ProtectedStack /> */}
     </NavigationContainer>
   );
 };
